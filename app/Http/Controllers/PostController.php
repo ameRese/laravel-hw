b81b46b5b620c05e3922edf0755511a7848e5ce3
ameRese <yarukix@gmail.com>
2024-09-09 21:53:24 +0900

    feat: 投稿個別表示・編集・削除機能追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
         ]);
         
         $validated['user_id'] = auth()->id();
-        
+
         $post = Post::create($validated);
         $request->session()->flash('message', '保存しました');
         return back();
@@ -27,5 +27,33 @@ class PostController extends Controller
     public function index() {
         $posts = Post::all();
         return view('post.index', compact('posts'));
+    }
+
+    public function show(Post $post) {
+        return view('post.show', compact('post'));
+    }
+
+    public function edit(Post $post) {
+        return view('post.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post) {
+        $validated = $request->validate([
+            'title' => 'required|max:20',
+            'body' => 'required|max:400',
+        ]);
+
+        $validated['user_id'] = auth()->id();
+
+        $post->update($validated);
+
+        $request->session()->flash('message', '更新しました');
+        return back();
+    }
+
+    public function destroy(Request $request, Post $post) {
+        $post->delete();
+        $request->session()->flash('message', '削除しました');
+        return redirect('post');
     }
 }
